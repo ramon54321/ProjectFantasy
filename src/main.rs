a745ce89c9754c8309497a5f0a9e77715090f3ca
Ramon Brand <ramon2009@gmail.com>
2022-06-09 08:19:17 +0300

    Add noise to world

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ mod graphics;
 mod grid_sweep;
 mod world;
 
+use fast_poisson::Poisson2D;
 use graphics::{GpuApp, GpuFixture, GpuFixtureCreateInfo, GpuInterface, Sweep};
 use grid_sweep::GridSweep;
 use image::{ImageBuffer, Pixel, RgbImage};
+use line_drawing::{Bresenham, Point};
+use spade::{DelaunayTriangulation, Point2, Triangulation};
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
@@ -14,6 +17,12 @@ use world::{World, HEIGHT, WIDTH};
 fn create_sweeps(gpu_interface: &GpuInterface, gpu_fixture: &GpuFixture) -> Vec<Box<dyn Sweep>> {
     let sweep_1 = Box::new(GridSweep::new(&gpu_interface, &gpu_fixture));
     vec![sweep_1]
+}
+
+fn draw_line(image: &mut RgbImage, a: Point<i32>, b: Point<i32>) {
+    for (x, y) in Bresenham::new(a, b) {
+        image.put_pixel(x as u32, y as u32, Pixel::from_channels(0, 255, 0, 0));
+    }
 }
 
 fn main() {
@@ -31,6 +40,63 @@ fn main() {
             );
         }
     }
+
+    //let points: Vec<[f64; 2]> = Poisson2D::new()
+    //.with_dimensions([WIDTH as f64, HEIGHT as f64], 35.0)
+    //.iter()
+    //.take(1000)
+    //.collect();
+
+    //points.iter().for_each(|point| {
+    //image.put_pixel(
+    //point[0] as u32,
+    //point[1] as u32,
+    //Pixel::from_channels(255, 0, 0, 0),
+    //);
+    //});
+
+    //let mut triangulation: DelaunayTriangulation<_> = DelaunayTriangulation::new();
+    //points.iter().for_each(|point| {
+    //triangulation
+    //.insert(Point2::new(point[0], point[1]))
+    //.unwrap();
+    //});
+
+    //let face = triangulation.voronoi_faces().take(18).last().unwrap();
+    //face.
+
+    //for x in 0..WIDTH {
+    //for y in 0..HEIGHT {
+    //let distance = f64::sqrt(face.distance_2(Point2::new(x as f64, y as f64))) * 5.0;
+    //image.put_pixel(
+    //x as u32,
+    //y as u32,
+    //Pixel::from_channels(distance as u8, distance as u8, distance as u8, 255),
+    //);
+    //}
+    //}
+
+    //for edge in triangulation.directed_edges() {
+    //let a = edge.from().position();
+    //let b = edge.to().position();
+    ////if a.is_none() || b.is_none() {
+    ////continue;
+    ////}
+    ////let a = a.unwrap();
+    ////let b = b.unwrap();
+    //if a.x >= WIDTH as f64 - 2.0 || a.x < 0.0 || a.y >= HEIGHT as f64 - 2.0 || a.y < 0.0 {
+    //continue;
+    //}
+    //if b.x >= WIDTH as f64 - 2.0 || b.x < 0.0 || b.y >= HEIGHT as f64 - 2.0 || b.y < 0.0 {
+    //continue;
+    //}
+    //draw_line(
+    //&mut image,
+    //(a.x as i32, a.y as i32),
+    //(b.x as i32, b.y as i32),
+    //);
+    //}
+
     image.save("world.png").unwrap();
 
     //let event_loop = EventLoop::new();
--- src/world.rs
+++ src/world.rs
@@ -1,31 +1,92 @@
-use noise::{NoiseFn, Perlin};
+use noise::{
+    BasicMulti, Billow, Fbm, HybridMulti, NoiseFn, OpenSimplex, Perlin, Select, SuperSimplex,
+    Terrace, Turbulence, Value, Worley,
+};
 
 pub const TILING: f64 = 8.0;
 pub const WIDTH: usize = 1024;
 pub const HEIGHT: usize = 1024;
 
+fn new_data_grid<T: Clone>(initial_value: T) -> Vec<Vec<T>> {
+    vec![vec![initial_value; HEIGHT]; WIDTH]
+}
+
+fn remap(x: f64, min: f64, max: f64, a: f64, b: f64) -> f64 {
+    let delta_in = max - min;
+    let delta_out = b - a;
+    (delta_out * ((x - min) / delta_in)) + a
+}
+
+fn min_in(collection: &Vec<Vec<f64>>) -> f64 {
+    let mut min_value = f64::INFINITY;
+    for x in 0..WIDTH {
+        for y in 0..HEIGHT {
+            if min_value > collection[x][y] {
+                min_value = collection[x][y];
+            }
+        }
+    }
+    min_value
+}
+
+fn max_in(collection: &Vec<Vec<f64>>) -> f64 {
+    let mut max_value = f64::NEG_INFINITY;
+    for x in 0..WIDTH {
+        for y in 0..HEIGHT {
+            if max_value < collection[x][y] {
+                max_value = collection[x][y];
+            }
+        }
+    }
+    max_value
+}
+
+fn remap_collection(collection: &mut Vec<Vec<f64>>, min: f64, max: f64) {
+    let min_value = min_in(&collection);
+    let max_value = max_in(&collection);
+    for x in 0..WIDTH {
+        for y in 0..HEIGHT {
+            let current_value = collection[x][y];
+            collection[x][y] = remap(current_value, min_value, max_value, min, max);
+        }
+    }
+}
+
+fn get_noise<F: NoiseFn<[f64; 2]>>(func: F) -> Vec<Vec<f64>> {
+    let mut values = new_data_grid(0.0);
+    for x in 0..WIDTH {
+        for y in 0..HEIGHT {
+            let x = x as f64;
+            let y = y as f64;
+            let regional_value =
+                func.get([(x * TILING) / WIDTH as f64, (y * TILING) / HEIGHT as f64]);
+            values[x as usize][y as usize] = regional_value;
+        }
+    }
+    values
+}
+
 pub struct World {
-    pub heights: [[u16; WIDTH]; HEIGHT],
+    pub heights: Vec<Vec<u16>>,
 }
 
 impl World {
     pub fn new() -> Self {
         Self {
-            heights: [[128; WIDTH]; HEIGHT],
+            heights: new_data_grid(0),
         }
     }
 
     pub fn generate(&mut self) {
         let perlin = Perlin::new();
+        let worley = Worley::new().enable_range(true);
+
+        let mut regional_values = get_noise(perlin);
+        remap_collection(&mut regional_values, 0.0, 1.0);
 
         for x in 0..WIDTH {
             for y in 0..HEIGHT {
-                let noise_value = perlin.get([
-                    x as f64 / (WIDTH as f64 / TILING),
-                    y as f64 / (HEIGHT as f64 / TILING),
-                ]);
-                let noise_value_normalized = (noise_value + 1.0) * 127.5;
-                self.heights[x][y] = noise_value_normalized as u16;
+                self.heights[x][y] = (regional_values[x][y] * 255.0) as u16;
             }
         }
 
